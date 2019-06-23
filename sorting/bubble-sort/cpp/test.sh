8f68e58e9a3d21dd55f1fbeed8e85301ef17c3b6
Franco Colmenarez <francoacg1@gmail.com>
2019-06-23 14:01:30 -0400

    Using docker to compile the bubble sort in C++

--- sorting/bubble-sort/cpp/test.sh
+++ sorting/bubble-sort/cpp/test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -e
-echo "Bubble Sort algorithm in C++ (50000 indexes). Time in milliseconds" > results.txt;
-echo "" >> results.txt;
+set -e;
+echo "Bubble Sort algorithm in GCC (Latest) (50000 indexes). Time in milliseconds" | tee results.txt;
+echo "" | tee -a results.txt;
 
-g++ main.cpp -o main -O3;
+echo "" > docker.log; echo "" >> docker.error;
+
+docker pull gcc:latest 1> docker.log 2> docker.error
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app gcc:latest g++ -o main main.cpp -O3;
 
 total=0;
 count=0;
@@ -14,12 +17,10 @@ do
     ./main;
     tt=$((($(date +%s%N) - $ts)/1000000));
     total=$((total+tt));
-    echo "Test N° $count time: $tt ms" >> results.txt;
+    echo "Test N° $count time: $tt ms" | tee -a results.txt;
 done
-echo "" >> results.txt;
-echo "Tested $count times" >> results.txt;
-echo "Total time: $total ms" >> results.txt;
+echo "" | tee -a results.txt;
+echo "Tested $count times" | tee -a results.txt;
+echo "Total time: $total ms"| tee -a results.txt;
 average=$((total/count));
-echo "Average: $average ms" >> results.txt;
-
-cat results.txt;
+echo "Average: $average ms"| tee -a results.txt;
